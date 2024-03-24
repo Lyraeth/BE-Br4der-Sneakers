ed609a815193f9bf6c420c080c80351f92d29048
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-03-24 15:07:22 +0700

    feat(order) : create routes CRUD for order

--- server.js
+++ server.js
@@ -6,17 +6,34 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const { prisma } = require("./config/prisma");
-const { userRoutes } = require("./routes/userRoutes");
-const { categoryRoutes } = require("./routes/categoryRoutes");
-const { productRoutes } = require("./routes/productRoutes");
-const { itemOrderRoutes } = require("./routes/itemOrderRoutes");
-const { userAdminRoutes } = require("./routes/userAdminRoutes");
+const {
+  prisma
+} = require("./config/prisma");
+const {
+  userRoutes
+} = require("./routes/userRoutes");
+const {
+  categoryRoutes
+} = require("./routes/categoryRoutes");
+const {
+  productRoutes
+} = require("./routes/productRoutes");
+const {
+  itemOrderRoutes
+} = require("./routes/itemOrderRoutes");
+const {
+  orderRoutes
+} = require("./routes/orderRoutes");
+const {
+  userAdminRoutes
+} = require("./routes/userAdminRoutes");
 
 // Callback <> biar Req App express kepake
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 // Create response for success route
 app.get("/", async (req, res) => {
@@ -44,6 +61,9 @@ app.use("/product", productRoutes);
 
 // itemOrderRoutes
 app.use("/itemOrder", itemOrderRoutes);
+
+// itemOrderRoutes
+app.use("/order", orderRoutes);
 
 // userAdminRoutes
 app.use("/userAdmin", userAdminRoutes);
