511356d87ef433c7d03395ecd91d1687299481a9
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2024-04-26 17:10:29 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,6 @@ function authenticateToken(req, res, next) {
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).send({ message: "Invalid token" });
     req.user = user;
-    next();
   });
 }
 
